6b9b1e4534175fc482e895f520dd0ccb331309c5
sbx_user1051 <sbx_user1051@169.254.59.197>
2024-06-05 20:14:49 +0000

    Wed Jun 05 20:14:49 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,10 +13,13 @@ skus = "ABBACDDA"
 
 def count_occurances(shopping_list):
 
-    # get the unique items from the shopping list by using a 
+    
+    # get the unique items from the shopping list by using a Set
+    uniques = Set(shopping_list)
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     
+
