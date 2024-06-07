2ca3b178296e7b80e99fdce5cdc92ad16a400d19
sbx_user1051 <sbx_user1051@169.254.117.17>
2024-06-07 20:41:25 +0000

    Fri Jun 07 20:41:25 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -148,40 +148,13 @@ def checkout(skus):
         
         item_data = data[key]
         price = item_data[0]
-        special_offer = item_data[1]
-        
-        # check if there's a special offer. If there is, calculate its effect on the bill
-        if special_offer:
-            
-            # get the quantity and inclusive price of the special offer
-            special_quantity, special_price = parse_special_offer(special_offer)
-            
-            # get the number of times this special offer can be accounted for, rounded down 
-            # to the nearest whole number
-            special_occurances = int(item_count / special_quantity)
 
-            # add the special offer pricing to the total price
-            total_price += special_occurances * special_price
-            
-            # take the special offer items out of the item count
-            item_count -= special_occurances * special_quantity
-
-        
-        # account for normal non special offer prices of items
-
-        total_price += item_count * price
 
     return total_price        
 
 
 
-special_offers = pre_process_discounts(data)
 
-for item in special_offers:
-    for offer in special_offers[item]:
-        print(offer)
-    
-    print("\n--")
 
 
 
--- lib/solutions/CHK/test.py
+++ lib/solutions/CHK/test.py
@@ -1,5 +1,29 @@
 dict = {"A": [],
         "B": []}
 
-if "A" in list(dict.keys()):
-    print("yes")
+# if "A" in list(dict.keys()):
+#     print("yes")
+
+
+#         special_offer = item_data[1]
+        
+#         # check if there's a special offer. If there is, calculate its effect on the bill
+#         if special_offer:
+            
+#             # get the quantity and inclusive price of the special offer
+#             special_quantity, special_price = parse_special_offer(special_offer)
+            
+#             # get the number of times this special offer can be accounted for, rounded down 
+#             # to the nearest whole number
+#             special_occurances = int(item_count / special_quantity)
+
+#             # add the special offer pricing to the total price
+#             total_price += special_occurances * special_price
+            
+#             # take the special offer items out of the item count
+#             item_count -= special_occurances * special_quantity
+
+        
+#         # account for normal non special offer prices of items
+
+#         total_price += item_count * price
