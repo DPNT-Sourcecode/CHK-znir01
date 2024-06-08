29091ff4b73b8dac166e44071691abfa72dee9f3
sbx_user1051 <sbx_user1051@169.254.117.17>
2024-06-08 01:02:32 +0000

    Sat Jun 08 01:02:32 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,6 +8,26 @@ data = {
     "D": [15, ""],
     "E": [40, "2E get one B free"],
     "F": [10, "2F get one F free"],
+    "G": [20, ""],
+    "H": [10, "5H for 45, 10H for 80"],
+    "I": [35, ""],
+    "J": [60, ""],
+    "K": [80, "2K for 150             |
+| L    | 90    |                        |
+| M    | 15    |                        |
+| N    | 40    | 3N get one M free      |
+| O    | 10    |                        |
+| P    | 50    | 5P for 200             |
+| Q    | 30    | 3Q for 80              |
+| R    | 50    | 3R get one Q free      |
+| S    | 30    |                        |
+| T    | 20    |                        |
+| U    | 40    | 3U get one U free      |
+| V    | 50    | 2V for 90, 3V for 130  |
+| W    | 20    |                        |
+| X    | 90    |                        |
+| Y    | 10    |                        |
+| Z    | 50    |                        |
 }
 
 # I am assuming you can't get more than 19 free items in an offer
@@ -208,6 +228,7 @@ def checkout(skus):
 
 
 print(checkout("FFFFFFFFF"))
+
 
 
 
