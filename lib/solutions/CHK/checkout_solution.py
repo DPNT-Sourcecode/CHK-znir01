78241120ed6ff69e54409190bd3ae0378784b493
sbx_user1051 <sbx_user1051@169.254.17.117>
2024-06-08 12:16:15 +0000

    Sat Jun 08 12:16:15 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -66,7 +66,6 @@ def pre_process_discounts(item_data):
 
             if offer: 
                 
-                
                 parsed_special_offer = parse_special_offer(item, offer_index, item_data)
                 discount_data[parsed_special_offer["discount target"]].append(parsed_special_offer)
 
@@ -268,6 +267,7 @@ def checkout(skus):
 pre_process_discounts(data)
 
 # print(data["Z"][0])
+
 
 
 
